cee0f85474ca6700186320eb3d421739bbd100ac
Askarjkee <arman_a@bankrbk.kz>
2023-10-20 17:59:05 +0600

    19

--- src/core/19/index.js
+++ src/core/19/index.js
@@ -6,7 +6,7 @@ class Result {
 		this.error = null;
 
 		try {
-			this.value = this.callback()
+			this.value = this.callback();
 			this.state = 'ok';
 		} catch (error) {
 			this.error = error;
@@ -23,21 +23,48 @@ class Result {
 
 	catch(callback) {
 		if (this.state === 'error') {
-			callback(this.error)
+			callback(this.error);
 		}
 	}
 }
 
-const res1 = new Result(() => 55);
+function exec(generator) {
+	const iterator = generator();
 
-res1.then((data) => {
-	console.log(data);
+	function handleResult (result) {
+		if (result instanceof Result) {
+			return result;
+		} else {
+			return new Result(() => result);
+		}
+	}
+
+	function iterate(iteration) {
+		if (iteration.done) {
+			return;
+		}
+
+		const result = iteration.value;
+
+		if (result instanceof Result) {
+			result
+				.then(value => iterate(iterator.next(handleResult(value))))
+				.catch(error => iterate(iterator.throw(error)))
+		} else {
+			iterate(iterator.next(handleResult(result)));
+		}
+	}
+
+	iterate(iterator.next());
+}
+
+exec(function* main() {
+	const res1 = new Result(() => 55);
+	console.log((yield res1).value);
+	try {
+		const res2 = new Result(() => { throw 'Boomx!'; });
+		yield res2; // этот код не будет выполнен из-за выброшенного исключения
+	} catch (err) {
+		console.error(err);
+	}
 });
-
-const res2 = new Result(() => { throw 'Boom!'; });
-
-res2
-	.then((data) => {
-	console.log('bla');
-	})
-	.catch((e) => console.error(e));
